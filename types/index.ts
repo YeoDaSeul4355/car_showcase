443612e8e5a203d70ac948854b0defdad6f285fb
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-02-20 23:21:36 +0900

    💬 설명 주석 추가

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,20 @@
 import { MouseEventHandler } from "react";
 
+// Home 속성
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+// API 요청시 파라미터 값 속성
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+// 자동차 속성(API 요청시)
 export interface CarProps {
   city_mpg: number;
   class: string;
@@ -15,18 +30,7 @@ export interface CarProps {
   year: number;
 }
 
-export interface FilterProps {
-  manufacturer?: string;
-  year?: number;
-  model?: string;
-  limit?: number;
-  fuel?: string;
-}
-
-export interface HomeProps {
-  searchParams: FilterProps;
-}
-
+// 자동차 카드에 나타낼 속성
 export interface CarCardProps {
   model: string;
   make: string;
@@ -37,6 +41,7 @@ export interface CarCardProps {
   cityMPG: number;
 }
 
+// 커스텀 버튼 속성
 export interface CustomButtonProps {
   isDisabled?: boolean;
   btnType?: "button" | "submit";
@@ -47,6 +52,7 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
+// 커스텀 필터 속성
 export interface OptionProps {
   title: string;
   value: string;
@@ -57,11 +63,13 @@ export interface CustomFilterProps {
   options: OptionProps[];
 }
 
+// 더보기 속성
 export interface ShowMoreProps {
   pageNumber: number;
   isNext: boolean;
 }
 
+// 제조사 검색 속성
 export interface SearchManuFacturerProps {
   manufacturer: string;
   setManuFacturer: (manufacturer: string) => void;
