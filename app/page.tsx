01f38804c9de4256aa90e6d8b614dd5108cd4aa3
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-02-20 23:54:06 +0900

    💬 설명 주석 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import { fetchCars } from "@/utils";
 import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
 export default async function Home({ searchParams }: HomeProps) {
+  // api 불러오는 부분
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
@@ -18,6 +17,7 @@ export default async function Home({ searchParams }: HomeProps) {
 
   return (
     <main className="overflow-hidden">
+      {/* 배너 부분 */}
       <Hero />
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
@@ -27,22 +27,27 @@ export default async function Home({ searchParams }: HomeProps) {
         </div>
 
         <div className="home__filters">
+          {/* 서치바 */}
           <SearchBar />
 
           <div className="home__filter-container">
+            {/* 연료, 연도 필터 부분 */}
             <CustomFilter title="fuel" options={fuels} />
             <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
+        {/* 자동차 데이터 매핑하는 곳(조건부 렌더링) */}
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
+                // 자동차 카드
                 <CarCard car={car} />
               ))}
             </div>
 
+            {/* 더보기 버튼 */}
             <ShowMore
               pageNumber={(searchParams.limit || 10) / 10}
               isNext={(searchParams.limit || 10) > allCars.length}
@@ -50,6 +55,7 @@ export default async function Home({ searchParams }: HomeProps) {
           </section>
         ) : (
           <div className="home__error-container">
+            {/* 자동차 데이터 없을 시 */}
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
