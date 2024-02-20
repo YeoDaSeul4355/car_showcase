732c855062243eff71e7932cc3632ed2a5f35377
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-02-20 18:06:17 +0900

    🎨 CustomFilter 수정

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -8,19 +8,28 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
-  const [selected, setSelected] = useState(options[0]);
+export default function CustomFilter({ title, options }: CustomFilterProps) {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
+
+  // update the URL search parameters and navigate to the new URL
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
 
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
-          setSelected(e);
-          setFilter(e.value);
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
         }}
       >
         <div className="relative w-fit z-10">
+          {/* Button for the listbox */}
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
             <Image
@@ -31,13 +40,15 @@ const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
               alt="chevron_up-down"
             />
           </Listbox.Button>
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
             <Listbox.Options className="custom-filter__options">
+              {/* Map over the options and display them as listbox options */}
               {options.map((option) => (
                 <Listbox.Option
                   key={option.title}
@@ -67,6 +78,4 @@ const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
       </Listbox>
     </div>
   );
-};
-
-export default CustomFilter;
+}
